3e7555b033d0a00828db6dc0bab20b4b1d82b699
Alexander Litvinov <allidemocracy@gmail.com>
2020-02-26 19:49:28 +0300

    add SKScene

---
+++ MyWether/ParticlesView.swift
@@ -0,0 +1,28 @@
+//
+//  ParticlesView.swift
+//  MyWeather
+//
+//  Created by Alexander on 26.02.2020.
+//  Copyright © 2020 Alexander Litvinov. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class ParticlesView: SKView {
+    override func didMoveToSuperview() {
+        let scene = SKScene(size: self.frame.size)
+        scene.backgroundColor = UIColor.clear
+        self.presentScene(scene)
+        
+        self.allowsTransparency = true
+        self.backgroundColor = UIColor.clear
+        
+        if let particles = SKEmitterNode(fileNamed: "ParticleScene.sks") {
+            particles.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height)
+            particles.particlePositionRange = CGVector(dx: self.bounds.size.width, dy: 0)
+            scene.addChild(particles)
+        }
+        
+    }
+}
